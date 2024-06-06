b370e8075fb851949027178803d73e97b9082640
Avery King <averyking345@gmail.com>
2024-06-05 20:19:25 -0500

    init

---
+++ Data/IPlatformRepo.cs
@@ -0,0 +1,9 @@
+namespace PlatformService.Models;
+
+public interface IPlatformRepo
+{
+    bool SaveChanges();
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformById(int id);
+    void CreatePlatform(Platform platform);
+}
